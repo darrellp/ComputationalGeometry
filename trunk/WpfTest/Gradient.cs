86e48268cc66a656f14aa57c694361a8b4cda6ec
darrellp <darrellp@8d1bb788-dd9b-7a4f-bf66-f19a0271ce37>
2011-03-01 10:20:09 +0000

    Initial Checkin

---
+++ trunk/WpfTest/Gradient.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace WpfTest
+{
+	internal class Gradient
+	{
+		private static readonly Random Rnd = new Random();
+		private SortedList<Double, Color> Stops { get; set; }
+
+		public Gradient()
+		{
+			Stops = new SortedList<double, Color> {{0, Colors.White}, {1, Colors.Black}};
+		}
+
+		private static byte Lerp(byte v1, byte v2, double t)
+		{
+			return (byte)(v1 + (v2 - v1)*t);
+		}
+
+		public bool AddStop(double pos, Color c)
+		{
+			if (pos <= 0 || pos >= 1 || Stops.ContainsKey(pos))
+			{
+				return false;
+			}
+			Stops[pos] = c;
+			return true;
+		}
+
+		public bool RemoveStop(double pos)
+		{
+			if (pos == 0 || pos == 1)
+			{
+				return false;
+			}
+
+			if (Stops.ContainsKey(pos))
+			{
+				Stops.Remove(pos);
+				return true;
+			}
+			return false;
+		}
+
+		public bool RemoveStop(int index)
+		{
+			if (index <= 0 || index >= Stops.Count - 1)
+			{
+				return false;
+			}
+			Stops.RemoveAt(index);
+			return true;
+		}
+
+		public bool ModifyStop(int index, Color c)
+		{
+			if (index < 0 || index >= Stops.Count)
+			{
+				return false;
+			}
+			// This is really odd - why can't I do this directly - i.e.,
+			// Stops.Values[index] = hsv?  It's just essentially a syntax change
+			// but I get an error on the above saying that I'm trying to "change
+			// the underlying SortedList".  Yes, I'm changing the value at a key
+			// which doesn't change the sort at all so why the big fuss?
+			Stops[Stops.Keys[index]] = c;
+			return true;
+		}
+
+		public bool ModifyStop(double pos, Color c)
+		{
+			if (pos < 0 || pos > 1 || !Stops.ContainsKey(pos))
+			{
+				return false;
+			}
+			Stops[pos] = c;
+			return true;
+		}
+
+		public void SetStart(Color c)
+		{
+			Stops[0] = c;
+		}
+
+		public void SetEnd(Color c)
+		{
+			Stops[1] = c;
+		}
+
+		public Color GetRandomColor()
+		{
+			var rnd = Rnd.NextDouble();
+			var clrRet = Colors.Black;
+
+			for (var i = 0; i < Stops.Count; i++)
+			{
+				if (Stops.Keys[i] > rnd)
+				{
+					var frac = (rnd - Stops.Keys[i-1])/(Stops.Keys[i] - Stops.Keys[i - 1]);
+					var clr1 = Stops.Values[i - 1];
+					var clr2 = Stops.Values[i];
+					clrRet = Color.FromRgb(
+						Lerp(clr1.R, clr2.R, frac),
+						Lerp(clr1.G, clr2.G, frac),
+						Lerp(clr1.B, clr2.B, frac)
+						);
+					break;
+				}
+			}
+			return clrRet;
+		}
+	}
+}
---
+++ trunk/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using DAP.CompGeom;
+using Geometry = DAP.CompGeom.Geometry;
+
+namespace WpfTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		readonly Random _rnd = new Random(23);
+
+		private IEnumerable<PointD> PolyCanvasExp()
+		{
+			return new List<PointD>
+			       	{
+			                         		new PointD(0, 0),
+			                         		new PointD(cvsMain.ActualWidth, 0),
+			                         		new PointD(cvsMain.ActualWidth, cvsMain.ActualHeight),
+			                         		new PointD(0, cvsMain.ActualHeight)
+			                         	};
+		}
+
+		private struct LevelInfo
+		{
+			public readonly Gradient GrdFill;
+			public readonly double StrokeThickness;
+			public readonly double Density;
+			public readonly byte Alpha;
+			public readonly int Relaxations;
+
+			public LevelInfo(
+				Gradient grdFill,
+				double strokeThickness,
+				double density,
+				byte alpha,
+				int relaxations)
+			{
+				GrdFill = grdFill;
+				StrokeThickness = strokeThickness;
+				Density = density;
+				Alpha = alpha;
+				Relaxations = relaxations;
+			}
+		}
+
+		private void SubVoronoi(IEnumerable<PointD> poly, IEnumerable<LevelInfo> ienInfo)
+		{
+			if (!ienInfo.Any() || !poly.Any())
+			{
+				return;
+			}
+
+			var info = ienInfo.First();
+			var pxLeft = double.MaxValue;
+			var pxRight = double.MinValue;
+			var pxBottom = double.MaxValue;
+			var pxTop = double.MinValue;
+
+			foreach (var pt in poly)
+			{
+				if (pt.X < pxLeft)
+				{
+					pxLeft = pt.X;
+				}
+				if (pt.X > pxRight)
+				{
+					pxRight = pt.X;
+				}
+				if (pt.Y < pxBottom)
+				{
+					pxBottom = pt.Y;
+				}
+				if (pt.Y > pxTop)
+				{
+					pxTop = pt.Y;
+				}
+			}
+
+			var bbWidth = pxRight - pxLeft;
+			var bbHeight = pxTop - pxBottom;
+			var ptUL = new PointD(pxLeft, pxTop);
+			var ptLR = new PointD(pxRight, pxBottom);
+			var cPts = (int)(bbWidth * bbHeight * info.Density * 100 / (cvsMain.ActualWidth * cvsMain.ActualHeight));
+			var lstPoints = new List<PointD>();
+
+			for (var i = 0; i < cPts ; i++)
+			{
+				var ptCand = new PointD(
+					_rnd.NextDouble() * bbWidth + pxLeft,
+					_rnd.NextDouble() * bbHeight + pxBottom);
+				if (Geometry.PointInConvexPoly(ptCand, poly))
+				{
+					lstPoints.Add(ptCand);
+				}
+			}
+			if (lstPoints.Count == 0)
+			{
+				lstPoints.Add(poly.First());
+			}
+			ptUL += new PointD(-1, 1);
+			ptLR += new PointD(1, -1);
+			var we = Fortune.ComputeVoronoi(lstPoints);
+			for (var iRelax = 0; iRelax < info.Relaxations; iRelax++)
+			{
+				we = Fortune.LloydRelax(we, 10000, poly, 1.0);
+			}
+
+			foreach (var subPoly in we.LstPolygons.Where(p => !p.FAtInfinity))
+			{
+				var testing = subPoly.RealVertices(ptUL, ptLR).ToArray();
+				var ptsClipped = ConvexPolyIntersection.FindIntersection(subPoly.RealVertices(ptUL, ptLR), poly);
+				//var ptsClipped = ConvexPolyIntersection.FindIntersection(subPoly.RealVertices(10000), poly);
+				if (Geometry.PointInConvexPoly(new PointD(1, 311), ptsClipped))
+				{
+					if (ptUL.X == 1000.3)
+						Console.WriteLine(testing.ToString());
+				}
+				var clrFill = info.GrdFill.GetRandomColor();
+				clrFill.A = info.Alpha;
+				var subPolyWpf = new Polygon
+				    {
+				        Points = new PointCollection(ptsClipped.Select(p => new Point(p.X, p.Y))),
+						Fill = new SolidColorBrush(clrFill),
+				        Stroke = new SolidColorBrush(Colors.Black),
+				        StrokeThickness = info.StrokeThickness
+					};
+				SubVoronoi(ptsClipped, ienInfo.Skip(1));
+				cvsMain.Children.Add(subPolyWpf);
+			}
+		}
+
+		private void NewDesign()
+		{
+			cvsMain.Children.Clear();
+			var grd = new Gradient();
+			grd.SetStart(Colors.Lavender);
+			grd.AddStop(0.5, Colors.Purple);
+			grd.SetEnd(Colors.SkyBlue);
+
+			var grd2 = new Gradient();
+			grd.SetStart(Colors.Red);
+			grd.SetEnd(Colors.Green);
+			//grd.SetStart(Colors.Red);
+			//grd.SetEnd(Colors.Red);
+			var li = new List<LevelInfo>
+			         	{
+										new LevelInfo(grd, 4, 0.4, 0, 2),
+										new LevelInfo(grd, 2, 1, 255, 0),
+										//new LevelInfo(grd, 5, 0.2, 50, 2),
+										//new LevelInfo(grd, 2, 3, 90, 1),
+										//new LevelInfo(grd, 1, 15, 150, 0),
+										//new LevelInfo(grd2, .5, 60, 255, 0)
+			                     	};
+			SubVoronoi(PolyCanvasExp(), li);
+			return;
+		}
+
+		private bool _fSizedOnce;
+
+		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (!_fSizedOnce)
+			{
+				NewDesign();
+				_fSizedOnce = true;
+			}
+		}
+
+		private void cvsMain_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			NewDesign();
+		}
+	}
+}
